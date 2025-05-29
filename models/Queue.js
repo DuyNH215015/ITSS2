b2bf47d84760a36c9b3b673dd41b7baa70c363e7
DuyNH215015 <duy362124.com>
2025-05-29 21:46:29 +0700

    fix

--- models/Queue.js
+++ models/Queue.js
@@ -2,8 +2,8 @@ module.exports = (sequelize, DataTypes) => {
   const Queue = sequelize.define("Queue", {
     queue_id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     status: { type: DataTypes.STRING }, // hoặc ENUM nếu cần
-    language: { type: DataTypes.ENUM("english", "japanese", "vietnamese") }, 
-    topic: { type: DataTypes.ENUM("daily", "job", "travel") }, 
+    language: { type: DataTypes.STRING}, 
+    topic: { type: DataTypes.STRING }, 
     level: { type: DataTypes.STRING },
   });
 
